5f08c97d1514b09360aa8163a7c76c04829b6ffc
codenamejewell <jewwri@gmail.com>
2025-02-12 10:29:18 -0500

    Created main.py for debugging

---
+++ main.py
@@ -0,0 +1,8 @@
+"""There are 3 errors in this code. Please use your own ability to find them all. Consider this a python refresher."""
+
+
+for i in range(5):
+    number = input("Enter a number: ")
+    total += number
+
+print("The running total is: "  total)
