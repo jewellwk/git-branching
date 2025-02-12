fde93d78fd4cf88f57bb8e5fd83148e24b626bba
codenamejewell <jewwri@gmail.com>
2025-02-12 10:29:43 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-"""There are 3 errors in this code. Please use your own ability to find them all. Consider this a python refresher."""
-
+"""There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
 
 for i in range(5):
     number = input("Enter a number: ")
